0498f994be0cff59d98150e7e49da16a1f80e9a5
uldershelby <full.diesel@gmail.com>
2022-09-05 15:48:20 -0300

    Working on 3rd screen (new game/quit)

--- script/main.js
+++ script/main.js
@@ -8,7 +8,31 @@ document.getElementById("btn-add-word").addEventListener(
     document.getElementById("add-word").innerHTML = "";
     //document.getElementById("result").style.backgroundImage = "none";
     document.getElementById("header").innerHTML =
-      '<textarea id="add-word-textarea" role="textbox" placeholder="Ingrese una palabra" autofocus required></textarea>';
+      '<textarea id="add-word-textarea" role="textbox" placeholder="Type a word" autofocus required></textarea>';
+    //document.getElementById("logo-alura").style.visibility = "visible";
+
+    if (hidden) {
+      element.removeAttribute("hidden");
+      element2.removeAttribute("hidden");
+    } else {
+      element.setAttribute("hidden", "hidden");
+      element2.setAttribute("hidden", "hidden");
+    }
+  },
+  false
+);
+
+document.getElementById("btn-save").addEventListener(
+  "click",
+  () => {
+    let element = document.getElementById("btn-new-game");
+    let element2 = document.getElementById("btn-quit");
+    let hidden = element.getAttribute("hidden");
+    document.getElementById("btn-save").style.visibility = "hidden";
+    document.getElementById("btn-cancel").style.visibility = "hidden";
+    //document.getElementById("result").style.backgroundImage = "none";
+    document.getElementById("header").innerHTML =
+      '<textarea id="add-word-textarea" role="textbox" placeholder="Type a word" autofocus required></textarea>';
     //document.getElementById("logo-alura").style.visibility = "visible";
 
     if (hidden) {
