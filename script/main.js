363bf27589f9a375194a74c441b6ec5e4fd24c27
uldershelby <full.diesel@gmail.com>
2022-09-09 18:27:45 -0300

    Lots of changes. Added keyboard and divs for word and canvas

--- script/main.js
+++ script/main.js
@@ -32,10 +32,9 @@ document.getElementById("btn-save").addEventListener(
     let element = document.getElementById("btn-new-game");
     let element2 = document.getElementById("btn-quit");
     let hidden = element.getAttribute("hidden");
+
     document.getElementById("btn-save").style.display = "none";
     document.getElementById("btn-cancel").style.display = "none";
-    document.getElementById("header").innerHTML =
-      '<canvas id="canvas-draw"></canvas>';
 
     if (hidden) {
       element.removeAttribute("hidden");
@@ -44,6 +43,49 @@ document.getElementById("btn-save").addEventListener(
       element.setAttribute("hidden", "hidden");
       element2.setAttribute("hidden", "hidden");
     }
+    secretWordLines();
   },
   false
 );
+
+let words = [
+  "programa",
+  "frontend",
+  "alura",
+  "oracle",
+  "beca",
+  "diseño",
+  "bootcamp",
+];
+
+let correctGuess = new Array();
+let wordToGuess = "";
+let tries = 0;
+
+const secretWordLines = () => {
+  secretword.innerHTML = "";
+  for (let i = 0; i < words[i].length; i++) {
+    secretword.innerHTML += `
+          <section id="letterplace">
+              <p id="location${i}"></p>
+          </section>
+        `;
+  }
+};
+
+const keyIn = (letter) => {
+  //let selKey = document.getElementById(`#key${letter}`);
+  if (wordToGuess.includes(letter)) {
+    for (let i = 0; i < wordToGuess; i++) {
+      if (wordToGuess[i] == letter) {
+        document.getElementById(`#location${i}`).innerHTML = letter;
+        correctGuess[i] = letter;
+      } else {
+        tries++;
+        drawStickMan(tries);
+      }
+    }
+  }
+};
+
+const drawStickMan = () => {};
